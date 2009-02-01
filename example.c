6a2e1d450541b5c983052f83200c4f3f4b9f78c4
AK <captainalex@gmail.com>
2009-01-31 19:16:07 -0500

    Here is another go

---
+++ example.c
@@ -0,0 +1,17 @@
+int
+main()
+{
+  struct Person hero = { 20, "Robin Hood", 191 };
+  struct Person sidekick;
+
+  sidekick.age = 31;
+  sidekick.name = "John Little"
+  sidekick.height_in_cm = 237;
+
+  printf("%s is %d years old and stands %dcm tall in his socks\n",
+         sidekick.name, sidekick.age, sidekick.height_in_cm);
+
+  printf( "He is often seen with %s.\n", hero.name );
+
+  return 0;
+}
