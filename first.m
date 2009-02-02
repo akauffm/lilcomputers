3d44497fea2b800f329559cca093c68421cf3738
AK <captainalex@gmail.com>
2009-02-02 15:30:34 -0500

    Final with comments

--- first.m
+++ first.m
@@ -1,12 +1,19 @@
 #import <Foundation/Foundation.h>
 
 @interface YourClass : NSObject
+
+// I had to redefine the goodbye variable as a string because
+// it wouldn't work as a void within NSLog as it was implemented
+// in the example.
+
 {
 	NSString* byestring;
 }
 - (NSString*) goodbye;
 
 @end
+
+
 
 @interface MyClass : NSObject
 {
@@ -17,6 +24,8 @@
 - (void) hello;
 
 @end
+
+
 
 @implementation YourClass
 
@@ -67,13 +76,18 @@
 	return myFloat;
 }
 
+// setter method
 - (void) setFriend:(YourClass*)buddy
 {
+	//I'm not sure whether this is necessary, given the
+	//autorelease in main() but just in case... 
+	
 	[buddy retain];
 	[friend release];
 	friend = buddy;
 }
 
+// getter method
 - (YourClass*)friend
 {
 	return friend;
@@ -102,6 +116,10 @@ int main(int argc, char**argv)
 	
 	[instance setMyFloat: 10.0f];
 	NSLog(@"new value is %f", [instance myFloat]);
+	
+	// The first %@ returns the class and a pointer to the instance.
+	// To make it prettier, I guess you could define method that 
+	// stores the friend object's name and returns it instead.
 	
 	NSLog(@"This is my friend %@ and he says '%@'", [instance friend], [[instance friend] goodbye]);
 	
